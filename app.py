b506ebd84995336dbe7c48f9865704fe409bb878
rutyeroh-star <rut.yeroh@gmail.com>
2026-04-27 17:21:52 -0400

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from openai import OpenAI
+import os
 
 # Configuración de la página
 st.set_page_config(page_title="Intérprete alvaDesc", page_icon="🧬")
